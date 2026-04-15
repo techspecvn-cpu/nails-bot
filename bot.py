4b639345242aaf5a822c3252a03b6428cd99a893
Александр Кабанов <aleksandrkabanov@MacBook-Pro-Aleksandr.local>
2026-04-15 23:55:33 +0300

    fix indent time block

--- bot.py
+++ bot.py
@@ -83,8 +83,7 @@ async def handle_message(message: Message):
     elif state == "date":
         if text in ["Записаться", "Поговорить", "Маникюр", "Педикюр", "Наращивание"]:
             await message.answer("Сначала введи дату 🙏 Например: 25 марта")
-        
-    return
+            return
 
         user_data[user_id]["date"] = text
         user_data[user_id]["state"] = "time"
